bc0901dff0ca3e8a59910d25b1687c84bd456312
ShadowNinja909 <Terrell.AlexisWilson@edu.sait.ca>
2024-12-08 23:32:20 -0700

    Fetch API data when initializing AddRecipePage

--- app/pages/addrecipe/page.js
+++ app/pages/addrecipe/page.js
@@ -1,9 +1,25 @@
+"use client";
+
+import { useState, useEffect } from 'react';
+
 import AddForm from '../../components/AddForm';
+import GetData from '@/app/api/GetData';
 
 export default function AddRecipePage() {
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        async function fetchData() {
+            const fetchedData = await GetData();
+            setData(fetchedData);
+        }
+
+        fetchData();
+    }, []);
+
     return (
         <main className='bg-blue-950 min-h-screen flex justify-center'>
-            <AddForm />
+            <AddForm recipeData={data} />
         </main>
     );
 }
