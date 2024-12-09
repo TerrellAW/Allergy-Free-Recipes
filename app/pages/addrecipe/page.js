ec804851a5bc8f3ae4945e370d589b1bb8e32eb6
ShadowNinja909 <Terrell.AlexisWilson@edu.sait.ca>
2024-12-08 22:08:55 -0700

    Updated frontend

--- app/pages/addrecipe/page.js
+++ app/pages/addrecipe/page.js
@@ -2,7 +2,7 @@ import AddForm from '../../components/AddForm';
 
 export default function AddRecipePage() {
     return (
-        <main className='bg-blue-950 min-h-screen'>
+        <main className='bg-blue-950 min-h-screen flex justify-center'>
             <AddForm />
         </main>
     );
