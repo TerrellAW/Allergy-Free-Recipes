19f17e67f1b0ad77883e68983b7cb758c8da2a11
ShadowNinja909 <Terrell.AlexisWilson@edu.sait.ca>
2024-12-09 22:35:26 -0700

    Adjusted home button position

--- app/pages/addrecipe/page.js
+++ app/pages/addrecipe/page.js
@@ -40,7 +40,7 @@ export default function AddRecipePage() {
 
     return (
         <main className='bg-blue-950 min-h-screen flex flex-col items-center'>
-            <Link href="/" className='max-h-12 p-2 px-4 bg-blue-100 text-center hover:bg-blue-300 text-blue-950 rounded-md'>Home</Link>
+            <Link href="/" className='max-h-12 p-2 px-4 mt-5 bg-blue-100 text-center hover:bg-blue-300 text-blue-950 rounded-md'>Home</Link>
             <AddForm recipeData={data} />
         </main>
     );
